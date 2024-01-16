7ef7c5c1f4c5cec191d6ec8d1c8d0c5f85f3ecda
Daniil Abdulov <abdulov.dany@yandex.ru>
2024-01-16 09:35:39 +0500

    Update authMiddleware.ts

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -2,6 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
+
+/*согласно документации amoCRM: 
+'Токен должен быть доступен только вашему приложению, 
+поэтому не рекомендуется сохранять его в cookies браузера,
+открытых конфигурационных файлах и т. п.
+
+Знаю, что сохранение токена таким способом не является хорошей практикой.
+'*/
 global.access_token = "";
 
 export default async function authMiddleware(
@@ -9,17 +17,21 @@ export default async function authMiddleware(
   res: Response,
   next: NextFunction
 ) {
+  //проверяем, установили ли мы токен доступа. 
   if (global.access_token) {
-    req.myAccessToken = global.access_token;
+    //Если установлен, то middleware переходит на leadsController
     return next();
   }
+  //Если не установлен, то отправляем запрос на его получение
   try {
     const subdomain = process.env.SUBDOMAIN;
+    //Проверка наличия субдомена
     if (!subdomain) {
       return res
         .status(401)
         .json({ message: "Субдомен не задан в переменных окружения" });
     }
+    //Данные для получения токена согласно документации amoCRM
     const link = `https://${subdomain}.amocrm.ru/oauth2/access_token`;
     const data = {
       client_id: process.env.CLIENT_ID,
@@ -28,13 +40,13 @@ export default async function authMiddleware(
       code: process.env.CODE,
       redirect_uri: process.env.REDIRECT_URL,
     };
-
     const response = await axios.post(link, data);
     if (response.status !== 200) {
       return res
         .status(401)
         .json({ message: "Ошибка получения токена доступа" });
     }
+    //в случае успешного получения токена, присваиваем его глобальному объекту
     global.access_token = response.data.access_token;
     return next();
   } catch (e) {
